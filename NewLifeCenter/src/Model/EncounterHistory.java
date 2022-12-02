cbb85abac200b687e60ed989144668818eda659e
Shivani Guglani <guglani.s@northeastern.edu>
2022-12-02 14:11:29 -0500

    commit05EncounterHistory

--- NewLifeCenter/src/Model/EncounterHistory.java
+++ NewLifeCenter/src/Model/EncounterHistory.java
@@ -4,16 +4,49 @@
  */
 package Model;
 
+import java.util.ArrayList;
+import java.util.Date;
+
 /**
  *
  * @author shivaniguglani
  */
 public class EncounterHistory extends Encounter{
     
+    private ArrayList<Encounter> encounterList; 
     
     public EncounterHistory(float bodyTemp, int bloodPressure, float weight, float height, 
-            String medicinePrescribed , String procedure , String diagnosis){
-        super(bodyTemp, bloodPressure, weight, height, medicinePrescribed, procedure , diagnosis);
+            String medicinePrescribed , String procedure , String diagnosis, Date encounterDate){
+        super(bodyTemp, bloodPressure, weight, height, medicinePrescribed, procedure , diagnosis, encounterDate);
+        this.encounterList = new ArrayList<Encounter>();
+    }
+
+    public ArrayList<Encounter> getEncounterList() {
+        return encounterList;
+    }
+
+    public void setEncounterList(ArrayList<Encounter> encounterList) {
+        this.encounterList = encounterList;
     }
     
+    public void addEncounter(Encounter newEncounter){
+        encounterList.add(newEncounter);        
+    }
+    
+    public void deleteEncounter(Encounter encount){
+        encounterList.remove(encount);
+    }
+    
+    // public ArrayList<Appointment> findAppointmentByCounsellor(String counsellorID, ArrayList<Appointment> listOfAppointments)
+    
+    public ArrayList<Encounter> findEncounterByDate(Date encounterDate, ArrayList<Encounter> encounterList){
+        ArrayList<Encounter> encounterListByDate = new ArrayList<Encounter>();
+        for (Encounter enc : encounterList)
+        {
+            if (enc.getEncounterDate().equals(encounterDate)){
+                encounterListByDate.add(enc);
+            }
+        }
+        return encounterListByDate;
+    }
 }
--- NewLifeCenter/src/Model/Patient.java
+++ NewLifeCenter/src/Model/Patient.java
@@ -10,12 +10,29 @@ package Model;
  */
 public class Patient extends Person{
     
+    private int patiendID;
     private EncounterHistory encounterHistory;
     
     public Patient(String firstName, String lastName, String buildingName, String unitNumber, String city, 
-            String state, int zipCode, String email, String password, int ssn, int age){
+            String state, int zipCode, String email, String password, int ssn, int age, int patientID){
         super(firstName, lastName, buildingName, unitNumber, city,  state, zipCode, email, password, ssn, age);
     }
-        
 
+    public int getPatiendID() {
+        return patiendID;
+    }
+
+    public void setPatiendID(int patiendID) {
+        this.patiendID = patiendID;
+    }
+
+    public EncounterHistory getEncounterHistory() {
+        return encounterHistory;
+    }
+
+    public void setEncounterHistory(EncounterHistory encounterHistory) {
+        this.encounterHistory = encounterHistory;
+    }
+        
+    
 }
