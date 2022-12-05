c9ea41dfdb76559aa628d4b0688d77840879e02a
sharmakartik11 <sharma.kartik1@northeastern.edu>
2022-12-05 00:37:09 -0500

    added ambulance driver models

---
+++ NewLifeCenter/src/NewLife/AmbulanceDriver/AmbulanceDriverDirectory.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package NewLife.AmbulanceDriver;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author kartik
+ */
+public class AmbulanceDriverDirectory {
+    ArrayList<AmbulanceDriver> ambulanceDriverList;
+
+    public AmbulanceDriverDirectory(ArrayList<AmbulanceDriver> ambulanceDriverList) {
+        this.ambulanceDriverList = ambulanceDriverList;
+    }
+
+    public ArrayList<AmbulanceDriver> getAmbulanceDriverList() {
+        return ambulanceDriverList;
+    }
+
+    public void setAmbulanceDriverList(ArrayList<AmbulanceDriver> ambulanceDriverList) {
+        this.ambulanceDriverList = ambulanceDriverList;
+    }
+    
+    public ArrayList<AmbulanceDriver> addAmbulanceDriver(AmbulanceDriver ambulanceDriver){
+        
+        ambulanceDriverList.add(ambulanceDriver);
+        return ambulanceDriverList;
+    }
+    public void removeAmbulanceDriver(AmbulanceDriver ambulanceDriver){
+        ambulanceDriverList.remove(ambulanceDriver);
+    }
+}
