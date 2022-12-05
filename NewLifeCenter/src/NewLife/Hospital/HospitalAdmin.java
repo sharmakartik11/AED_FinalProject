0cfe1f24795c85af9ed2f9bb5eae82d5135af3e5
sharmakartik11 <sharma.kartik1@northeastern.edu>
2022-12-05 01:16:28 -0500

    model classes for hospital added

---
+++ NewLifeCenter/src/NewLife/Hospital/HospitalAdmin.java
@@ -0,0 +1,53 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package NewLife.Hospital;
+
+/**
+ *
+ * @author kartik
+ */
+public class HospitalAdmin {
+    private String hospitalName;
+    private String address;
+    private String orderId;
+    private String contactNumber;
+
+    public String getHospitalName() {
+        return hospitalName;
+    }
+
+    public void setHospitalName(String hospitalName) {
+        this.hospitalName = hospitalName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(String orderId) {
+        this.orderId = orderId;
+    }
+
+    public String getContactNumber() {
+        return contactNumber;
+    }
+
+    public void setContactNumber(String contactNumber) {
+        this.contactNumber = contactNumber;
+    }
+    
+    @Override
+    public String toString() {
+        return hospitalName;
+    }
+}
