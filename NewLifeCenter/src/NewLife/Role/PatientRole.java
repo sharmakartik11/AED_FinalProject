7373f616643909900fce2a4b66b0e9150d503376
Shivani Datar <shivanidatar@Shivanis-MacBook-Pro.local>
2022-12-08 14:01:58 -0500

    Added model classes and enum

---
+++ NewLifeCenter/src/NewLife/Role/PatientRole.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package NewLife.Role;
+
+import NewLife.UserAccount.UserAccount;
+import NewLifeCenter.NewLife;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author shivanidatar
+ */
+public class PatientRole extends Role{
+
+    @Override
+    public JPanel createWorkArea(JPanel userProcessContainer, UserAccount account, NewLife business) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    
+    
+}
